d88783b27ff6c23403ef615449f4a44df850190d
Sizzad Hosen <sizzadhosenofficial@gmail.com>
2025-07-23 21:05:14 +0600

    add chnage

--- src/app/dashboard/services/create-services/page.tsx
+++
@@ -1,9 +0,0 @@
-import { CreateServicePage } from "@/pages/Services/CreateService"
-
-const CreateService = () => {
-  return (
-    <CreateServicePage></CreateServicePage>
-  )
-}
-
-export default CreateService
